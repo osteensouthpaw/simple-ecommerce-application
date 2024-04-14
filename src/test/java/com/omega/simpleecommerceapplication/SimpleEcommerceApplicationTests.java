f500aecbe796db2e705995b06b74f664e5f81c87
osteensouthpaw <osteensouthpaw@gmail.com>
2024-04-14 07:23:29 +0300

    initial commit

---
+++ src/test/java/com/omega/simpleecommerceapplication/SimpleEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omega.simpleecommerceapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
